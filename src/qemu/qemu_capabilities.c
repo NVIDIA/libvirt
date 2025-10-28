c014b1e15ab79ac5731cb06811ed0a383089ebfe
Nathan Chen <nathanc@nvidia.com>
2025-10-28 20:38:49 +0000

    NVIDIA: SAUCE: qemu: Add qemu CLI support for EGM
    Add qemu CLI support for EGM memory device model:
    - Specify EGM device path to memory-backend-file object
    - Support acpi-egm-memory object with id, pci-dev, and
      node attributes
    - Consolidate all acpi-egm-memory objects' memory into
      a single memory-backend-file per EGM chardev
      specified.
    
    Signed-off-by: Ian May <ianm@nvidia.com>
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>
    (cherry-picked from commit bd4ffe017b86e00198a8186c2d0ca1ebcbdab06b https://github.com/NathanChenNVIDIA/libvirt/commits/egm-11-24-25/)
    Link: https://lists.libvirt.org/archives/list/devel@lists.libvirt.org/thread/DQHOGVDNU35RYSSNBKLXH2XQBROJXE7Z/
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>
    (cherry-picked from commit afd048e223342120d1efef26197b324d70c7656f https://github.com/NVIDIA/libvirt/commits/nvidia_stable-11.9)
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -746,6 +746,7 @@ VIR_ENUM_IMPL(virQEMUCaps,
 
               /* 485 */
               "acpi-generic-initiator", /* QEMU_CAPS_ACPI_GENERIC_INITIATOR */
+              "acpi-egm-memory", /* QEMU_CAPS_DEVICE_ACPI_EGM_MEMORY */
               "rme-guest", /* QEMU_CAPS_CCA_GUEST */
     );
 
@@ -1443,6 +1444,7 @@ struct virQEMUCapsStringFlags virQEMUCapsObjectTypes[] = {
     { "tpm-emulator", QEMU_CAPS_DEVICE_TPM_EMULATOR },
     { "tpm-passthrough", QEMU_CAPS_DEVICE_TPM_PASSTHROUGH },
     { "acpi-generic-initiator", QEMU_CAPS_ACPI_GENERIC_INITIATOR },
+    { "acpi-egm-memory", QEMU_CAPS_DEVICE_ACPI_EGM_MEMORY },
     { "rme-guest", QEMU_CAPS_CCA_GUEST },
 };
 
