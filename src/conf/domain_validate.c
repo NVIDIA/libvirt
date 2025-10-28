6e22881d1d9cfdc56f079941a6ac2b35e4b9426c
Nathan Chen <nathanc@nvidia.com>
2025-10-28 20:07:59 +0000

    NVIDIA: SAUCE: conf: Support EGM memory device model
    Add support for EGM memory device model with
    'path' source attribute and 'pciDev' target
    attribute to denote host EGM device backing path
    and PCI device alias to associate the vEGM with,
    respectively.
    
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>
    (cherry-picked from commit 4c20fdfd454d2343c3b7f576c33e72acecfd9904 https://github.com/NathanChenNVIDIA/libvirt/commits/egm-11-24-25/)
    Link: https://lists.libvirt.org/archives/list/devel@lists.libvirt.org/thread/DQHOGVDNU35RYSSNBKLXH2XQBROJXE7Z/
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>
    (cherry picked from commit 49528751def4020f76949f2615ec54bec1a5c271 nvidia_stable-11.9)
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Ian May <ianm@nvidia.com>
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>

--- src/conf/domain_validate.c
+++ src/conf/domain_validate.c
@@ -2564,6 +2564,7 @@ virDomainMemoryDefCheckConflict(const virDomainMemoryDef *mem,
         }
         break;
     case VIR_DOMAIN_MEMORY_MODEL_SGX_EPC:
+    case VIR_DOMAIN_MEMORY_MODEL_EGM:
     case VIR_DOMAIN_MEMORY_MODEL_NONE:
     case VIR_DOMAIN_MEMORY_MODEL_LAST:
         break;
@@ -2610,6 +2611,7 @@ virDomainMemoryDefCheckConflict(const virDomainMemoryDef *mem,
         switch (other->model) {
         case VIR_DOMAIN_MEMORY_MODEL_NONE:
         case VIR_DOMAIN_MEMORY_MODEL_SGX_EPC:
+        case VIR_DOMAIN_MEMORY_MODEL_EGM:
         case VIR_DOMAIN_MEMORY_MODEL_LAST:
             continue;
             break;
@@ -2786,6 +2788,19 @@ virDomainMemoryDefValidate(const virDomainMemoryDef *mem,
             virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
                            _("memory device address is not supported for model '%1$s'"),
                            virDomainMemoryModelTypeToString(mem->model));
+            return -1;
+        }
+        break;
+
+    case VIR_DOMAIN_MEMORY_MODEL_EGM:
+        if (!mem->source.egm.path) {
+            virReportError(VIR_ERR_XML_DETAIL, "%s",
+                           _("path is required for model 'egm'"));
+            return -1;
+        }
+        if (!mem->target.egm.pciDev) {
+            virReportError(VIR_ERR_XML_DETAIL, "%s",
+                           _("pciDev is required for model 'egm'"));
             return -1;
         }
         break;
