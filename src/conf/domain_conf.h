e0101140bb366fb37047a23c74ee86f87f4c2b7c
Ian May <ianm@nvidia.com>
2025-11-24 14:30:28 -0800

    NVIDIA: SAUCE: qemu: Add shared-mec support for ARM CCA guests
    Add support for configuring Memory Encryption Context (MEC) sharing
    behavior for ARM Confidential Compute Architecture (CCA) Realms.
    
    This adds a new 'shared-mec' attribute to the <launchSecurity type='cca'>
    element that controls whether Realms share encryption contexts:
    
    - shared-mec='yes': Realms share encryption contexts for efficiency
      with reduced isolation. Falls back gracefully to MECID 0 if MEC
      is unsupported.
    
    - shared-mec='no': Each Realm gets a dedicated encryption
      key for strong isolation. Requires kernel and RMM MEC support.
    
    Example:
      <launchSecurity type='cca' shared-mec='yes'>
        <measurement-algo>sha256</measurement-algo>
        <personalization-value>...</personalization-value>
      </launchSecurity>
    
    This attribute is passed to QEMU as 'shared-mec' on the 'rme-guest'
    object, matching the QEMU implementation added in commit
    ("hw/arm/virt: Define MEC as shared or private").
    
    Signed-off-by: Ian May <ianm@nvidia.com>
    (cherry picked from commit e08ccf2a435a397503fcaf5c60778fe5aaa11b5a nvidia_unstable-11.9)
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Ian May <ianm@nvidia.com>
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -3068,6 +3068,7 @@ struct _virDomainCCADef {
     char *measurement_algo;
     char *personalization_value;
     virTristateBool measurement_log;
+    virTristateBool shared_mec;
 };
 
 struct _virDomainSecDef {
